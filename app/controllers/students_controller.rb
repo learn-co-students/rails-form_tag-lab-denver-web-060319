4c25968f6d5612c018e0b3079e00d7fc10118ce5
Fekade Senbeta <fekadesenbeta@gmail.com>
2020-03-21 23:52:45 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,5 +7,17 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+  
+  def new
+  end
+
+  def create
+    @student = Student.new
+    @student.first_name = params[:student][:first_name]
+    @student.last_name = params[:student][:last_name]
+    @student.save
+    redirect_to student_path(@student)
+  end
+
 
 end
